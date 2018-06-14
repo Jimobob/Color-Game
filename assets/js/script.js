69ed92568c469d2b25ad5f6cfcf54a897fb53f72
Jimmy <JimmyCao227@yahoo.com>
2018-06-14 16:26:29 -0400

    Added difficulty level to the game, refined several things to improve interactivity.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var colorArray = [];
 var primaryColor;
 var previousColor;
+var hardLevel = true;
+var left = document.querySelector("#left");
+var checker = document.querySelector("#check");
 
 function randomNum(){
 	return Math.floor(Math.random()*256);
@@ -11,41 +14,75 @@ function generateRGB(){
 }
 
 function generateColorArray(){
-	for(var i = 0; i < 6; i++){
-		colorArray.push(generateRGB());
+	if (hardLevel){
+		console.log("hi");
+		for(var i = 0; i < 6; i++){
+			colorArray.push(generateRGB());
+		}
+	}
+
+	else{
+		console.log("yesss");
+		for(var i = 0; i < 3; i++){
+			colorArray.push(generateRGB());
+		}
 	}
 }
 
 function removeColorArray(){
-	for(var i = 0; i < 6; i++){
-		colorArray.pop();
+	if (hardLevel){
+		for(var i = 0; i < 6; i++){
+			colorArray.pop();
+		}
 	}
+
+	else{
+		for(var i = 0; i < 3; i++){
+			colorArray.pop();
+		}
+	}	
 }
 
 function colorDivs(){
 	generateColorArray();
-	primaryColor = colorArray[Math.floor(Math.random()*6)];
+	if (hardLevel){
+		primaryColor = colorArray[Math.floor(Math.random()*6)];
+	}
+	else{
+		primaryColor = colorArray[Math.floor(Math.random()*3)];
+	}
 	var divsToColor = document.querySelectorAll(".color");
 	var header = document.querySelector("#header");
-	var checker = document.querySelector("#check")
+	console.log(colorArray.length);
 	for(var i = 0; i < colorArray.length; i++){
 		divsToColor[i].style.backgroundColor = colorArray[i];
-		console.log(colorArray[i]);
+
+		if (!hardLevel){
+			for(var a = 3; a < 6; a++){
+				divsToColor[a].style.backgroundColor = "#2a2a2a";
+			}
+		}
 
 		divsToColor[i].addEventListener("click", function(){
 			if(this.style.backgroundColor == primaryColor){
+				console.log("hi");
 				header.style.backgroundColor = primaryColor;
-				console.log(primaryColor);
+				checker.innerHTML = "CORRECT!";
+				
 				var list = document.getElementsByTagName("li");
 				for(var a = 0; a < list.length; a++){
 					list[a].style.color = primaryColor;
 				}
 				
-				removeColorArray();
-				colorDivs();
+				for(var a = 0; a < colorArray.length; a++){
+					divsToColor[a].style.backgroundColor = primaryColor;
+				}
 
+				left.innerHTML = "PLAY AGAIN?";
 			}
 			else{
+				checker.innerHTML= "TRY AGAIN";
+				this.style.backgroundColor = "#2a2a2a";
 			}
 		})
 		var rgb = document.querySelector("#rgb");
@@ -58,7 +95,28 @@ function colorDivs(){
 
 var change = document.querySelector("#left");
 change.onclick = function(){
+	checker.innerHTML = " ";
+	left.innerHTML = "CHANGE COLOR";
 	removeColorArray();
+	colorDivs();
+}
+
+var easyOption = document.querySelector("#easy");
+var hardOption = document.querySelector("#hard");
+
+function easy(){
+	easyOption.style.backgroundColor = "#efefef";
+	hardOption.style.backgroundColor = "#fcfcfc";
+	removeColorArray();
+	hardLevel = false;
+	colorDivs();
+}
+
+function hard(){
+	hardOption.style.backgroundColor = "#efefef";
+	easyOption.style.backgroundColor = "#fcfcfc";
+	removeColorArray();
+	hardLevel = true;
 	colorDivs();
 }
 
